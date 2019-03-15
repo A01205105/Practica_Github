7fce84affb2ccdf2725b29e5a142f91482aae345
Valdemar <valde1hs@hotmail.com>
2019-03-15 10:34:59 -0600

    Agregando carpeta de out

---
+++ out/Prueba_v1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define VARIABLE 0
+#define VARIABLE 1
+#define VARIABLE 2 
+
+#define PATH "proc\kmsg"
+int main (void)
+{
+	return 0;
+}
+
+/* Control de Cambios
+01 | Se agregó una librería			| Valdemar Hernández 15/03/19
+02 | Se agregó una nueva variable	| Valdemar Hernández 15/03/19
+03 | Se agregó una tercera variable	| Valdemar Hernández 15/03/19
+*/
