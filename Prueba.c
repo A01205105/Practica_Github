f21dc1a2d5fdcb2b300ff9d919a4b6a4dc9172f0
Valdemar <valde1hs@hotmail.com>
2019-03-15 09:54:14 -0600

    Este es el primer Commit

---
+++ Prueba.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define VARIABLE 0
+#define VARIABLE 1
+
+#define PATH "proc\kmsg"
+int main (void)
+{
+	return 0;
+}
+
+/* Control de Cambios
+01 | Se agregó una librería			| Valdemar Hernández 15/03/19
+02 | Se agregó una nueva variable	| Valdemar Hernández 15/03/19
+*/
